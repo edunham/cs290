bea6b1e6e47bb83f9bfa2bf559f1fe51b4150881
edunham <github@edunham.net>
2015-10-09 21:14:23 -0700

    Add the divs

--- html_css_assignment/content.py
+++ html_css_assignment/content.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python2
 import multiplicationtable
+import listentries
 
 print """
 <!DOCTYPE html>
@@ -12,7 +13,19 @@ print """
     <body>
 """
 multiplicationtable.print_table()
+listentries.print_list()
 
+print """
+    <div id="outer-content">
+        We interrupt our program to bring you a haiku written by a robot:
+        <div id="inner-content">
+            <p>0 1 1 1</p>
+            <p>0 1 0 0</p> 
+            <p>1 1 0 1</p>
+        </div>
+        It's only a haiku if you read it out loud in English, though.
+    </div>
+"""
 
 print """
     </body>
