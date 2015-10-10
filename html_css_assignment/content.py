c33591800b7c64542be4a5a32a5347d54caef764
edunham <github@edunham.net>
2015-10-09 20:53:39 -0700

    Content generator script.

---
+++ html_css_assignment/content.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python2
+import multiplicationtable
+
+print """
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>HTML/CSS Assignment</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+"""
+multiplicationtable.print_table()
+
+
+print """
+    </body>
+</html>
+"""
