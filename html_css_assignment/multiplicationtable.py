63829cd3ff7448c8c1d8a87b3053eb5beec13e55
edunham <github@edunham.net>
2015-10-09 20:53:52 -0700

    Meet italics requirements

--- html_css_assignment/multiplicationtable.py
+++ html_css_assignment/multiplicationtable.py
@@ -15,15 +15,18 @@ def tr(cell, data):
     # cell is the string td or the string th
     # data is an iterable full of things with str()
     print "\t\t<tr>"
-    print "\t\t\t<th>" + str(data[0]) + "</th>"
+    print "\t\t\t<th class=it>" + str(data[0]) + "</th>"
     for d in data[1:]:
         print "\t\t\t<" + cell + ">" + str(d) + "</" + cell + ">"
     print "\t\t</tr>"
 
-print "\t<table>"
-print "\t<caption>Multiplication Table</caption>"
-timesby = [50,82,9,37,4]
-tr("th", ['']+timesby)
-for t in timesby:
-    tr("td", [t]+[t * b for b in timesby])
-print "\t</table>"
+def print_table():
+    print "\t<table>"
+    print "\t<caption>Multiplication Table</caption>"
+    timesby = [50,82,9,37,4]
+    times = [22, 7, 89, 40, 3]
+    tr("th", ['']+timesby)
+    for t in times:
+        tr("td", [t]+[t * b for b in timesby])
+    print '<tr><td colspan="2">two</td><td colspan="4">four</td></tr>'
+    print "\t</table>"
